6484d615912adc75bbee38fbf0e873b80af0e6ee
hirrumi <BlueberryRainbows@yandex.ru>
2023-10-14 14:42:47 +0300

    Add_new_func

--- str_func.py
+++ str_func.py
@@ -2,3 +2,8 @@ def foo():
     string = input("Введите ваш текст ").upper()
     return string
 
+
+def foo_2():
+    """Эта функция принимает строку и возвращает её переводя первый символ в верхний регистр"""
+    string = input("Введите ваш текст ").title()
+    return string
